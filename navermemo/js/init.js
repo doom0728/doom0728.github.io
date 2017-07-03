fccb80e19b709dbfe02d268fae2617a8a0e3a9ba
doom0728 <doom87@naver.com>
2017-07-03 13:58:15 +0900

    Naver memo

---
+++ navermemo/js/init.js
@@ -0,0 +1,18 @@
+$('.main_slider').bxSlider({
+    pagerCustom:'.pager_wrap',
+    controls: true,
+    nextSelector : '.btn_next',
+    prevSelector : '.btn_prev'
+});
+
+$('.com_slider').bxSlider({
+	mode : 'vertical',
+	pager:false,
+	nextSelector : '.btn_com_next',
+	prevSelector : '.btn_com_prev',
+	minSlides : 5,
+	moveSlides : 1,
+	auto : true,
+	pause : 4000
+	
+});
